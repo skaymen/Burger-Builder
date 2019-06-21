d1df66c7660b403306f0e349ee9c93fdcf716db9
Stan Kaymen <stanley.kaymen@gmail.com>
2019-06-21 16:25:44 -0400

    initial commit; functional app with no side effects

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component {
+    // This could be a functional component unless using lifecycle hook
+
+    // componentDidUpdate() {
+    //     console.log("[OrderSummary.js] did update");
+    // }
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{textTransform: 'capitalize'}}>{igKey}: {this.props.ingredients[igKey]}</span>
+                    </li>
+            )});
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        )
+    }
+};
+
+export default OrderSummary;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.SideDrawer}
+        });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer 
+                    closed={this.sideDrawerClosedHandler}
+                    open={this.state.showSideDrawer}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+
+export default Layout;
