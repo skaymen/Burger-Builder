faf2db3561139c6c5933b4a31691bc664b3fe7d2
Stan Kaymen <stanley.kaymen@gmail.com>
2019-06-28 18:44:49 -0400

    add redux to store ingredients and price

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
@@ -22,7 +23,7 @@ class ContactData extends Component {
         event.preventDefault();
         this.setState({ loading: true });
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             customer: {
                 name: 'Stas',
@@ -66,4 +67,11 @@ class ContactData extends Component {
 
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+};
+
+export default connect(mapStateToProps)(ContactData);
